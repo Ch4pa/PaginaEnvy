43c06a5fda3c0481245cc6a2bb836ee9fca7b8da
Mateo Sabelli <mateosabelli4a@gmail.com>
2023-01-02 15:30:00 -0300

    ultimo

---
+++ Js/index.js
@@ -0,0 +1,86 @@
+// let ropaElegida = prompt(`Que estas buscando?`)
+// console.log(ropaElegida.toLowerCase())
+
+
+// if ((ropaElegida.toLowerCase() == `buzos`) || (ropaElegida.toLowerCase() == `buzo`)) {
+//     alert(`Usteded puede comprar ${ropaElegida} en nuestra Pagina`);
+// }
+
+// else if ((ropaElegida.toLowerCase() == `remeras`) || (ropaElegida.toLowerCase() == `remera`)) {
+//     alert(`Usted puede comprar ${ropaElegida} en nuestra Pagina`);
+// }
+
+// else if (ropaElegida.toLowerCase() == `medias`) {
+//     alert(`No hay ${ropaElegida}`);
+// }
+// else if (ropaElegida == ``); {
+//     alert(`Ingrese nuevamente el producto`);
+// }
+
+
+// Corroborar talle
+
+// let corroborarTalle = prompt(`Queres ver si hay ${ropaElegida} de tu talle?`)
+// console.log(corroborarTalle.toLowerCase())
+// if (corroborarTalle == `si`) {
+//      prompt(`Que talle de ${ropaElegida} eres? (S, M, L, XL )`)
+// }
+// else{
+
+// }
+
+// let cantidadRopa = parseInt(prompt(`Cuantos ${ropaElegida} estas buscando?`))
+// console.log(cantidadRopa)
+
+/* let cantidadRopa = parseInt(prompt(`Cuantos productos estas buscando?`))
+ for (let i = 1; i <= cantidadRopa; i++){
+     let producto = prompt( `Ingrese el producto N*${i} `)
+     console.log( `${producto}`)
+     alert(`la cantidad de produtos son ${cantidadRopa} y son ${producto}`)
+     let talle = prompt (`Ingrese tu talle de ${producto}`)
+ console.log(talle)
+ } */
+
+
+// let entrada = prompt( `Ingrese un dato`)
+
+// while( entrada.toLowerCase() != `esc`){
+//     alert( `El usuario ingreso ${entrada}`)
+
+//     entrada= prompt( `Ingrese otro dato`)
+// }
+
+/* let numero = 0;
+do {
+    //Repetimos con do...while mientras el usuario ingresa un n°
+    numero = prompt("Ingresar Número");
+    console.log(numero);
+    //Si el parseo no resulta un número se interrumpe el bucle.   
+} while (parseInt(numero)); */
+
+// Estas buscando algun tipo de ropa en especial?,
+// condicional de cada tipo
+// Que talle de tal sos?
+
+    /* let ropa = prompt(`Ingrese un producto`) */
+
+/*     while (ropa.toLowerCase() != `ESC`) {
+        switch (ropa) {
+            case `buzos`:
+                alert(`Usted puede comprar buzos en nuestra Pagina!`);
+                break;
+            case `remeras` || `remera`:
+                alert(`Usted puede comprar remeras en nuestra Pagina!`);
+                break;
+            default:
+                alert(`No vendemos ese Producto`)
+                break;
+        }
+    } */
+
+
+
+let ganador = false
+while( !ganador){
+    let adivinanza = prompt(`Que fdf`)
+}
